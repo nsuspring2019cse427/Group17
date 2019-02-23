1bbd8127db56f453ab2189b755009e21a26229c1
Md. Amdadul Bari <amdadul.bari@northsouth.edu>
2019-02-23 21:44:16 +0600

    Project Initialized.

---
+++ Project1/Code/src/main/java/com/amdadulbari/krishokiot/KrishokiotApplication.java
@@ -0,0 +1,13 @@
+package com.amdadulbari.krishokiot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrishokiotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KrishokiotApplication.class, args);
+    }
+
+}
