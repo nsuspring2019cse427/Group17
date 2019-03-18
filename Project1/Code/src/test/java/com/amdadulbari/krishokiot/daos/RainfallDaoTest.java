99991b1b2cf55aad5f0f0cc98affcdffa4a7c4c9
RedwanRabbiHoque <riasat.rrh@gmail.com>
2019-03-19 01:09:26 +0600

    RainfallDao Test Parameters added.

--- Project1/Code/src/test/java/com/amdadulbari/krishokiot/daos/RainfallDaoTest.java
+++ Project1/Code/src/test/java/com/amdadulbari/krishokiot/daos/RainfallDaoTest.java
@@ -26,7 +26,17 @@ public class RainfallDaoTest {
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][]{
-                {new RainfallModel(0, 0, 98.34f), true}, {new RainfallModel(0, 0, 98.34f), true}
+                {new RainfallModel(0, 0, 98.34f), true},
+                {new RainfallModel(1, 0, 56.20f), true},
+                {new RainfallModel(2, 0, 44.50f), true},
+                {new RainfallModel(3, 0, 99.99f), true},
+                {new RainfallModel(4, 0, 0.00f), false},
+                {new RainfallModel(5, 0, 56.78f), true},
+                {new RainfallModel(6, 0, 33.33f), true},
+                {new RainfallModel(7, 0, 77.78f), true},
+                {new RainfallModel(8, 0, 48.14f), true},
+                {new RainfallModel(9, 0, 11.12f), true},
+                {new RainfallModel(10, 0, 29.69f), true},
         });
     }
 
